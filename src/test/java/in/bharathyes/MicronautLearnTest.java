7dcda4e5f7866f689548518b0ce8851d1a4853d6
Bharath S <bharathyes>
2023-01-29 19:12:17 +0000

    Initial commit

---
+++ src/test/java/in/bharathyes/MicronautLearnTest.java
@@ -0,0 +1,21 @@
+package in.bharathyes;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautLearnTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
